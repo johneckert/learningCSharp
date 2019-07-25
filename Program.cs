d5dbb3bab50a816ed70e0d7b01288ce617f44228
johneckert <johnteckert@gmail.com>
2019-07-25 14:59:59 -0400

    finished first lesson

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace learningCSharp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("What is your name?");
+      var name = Console.ReadLine();
+      Console.WriteLine("How old are you?");
+      var age = Console.ReadLine();
+      Console.WriteLine("In what month were you born?");
+      var month = Console.ReadLine();
+      Console.WriteLine($"Name: {name}");
+      Console.WriteLine($"Age: {age}");
+      Console.WriteLine($"Month: {month}");
+    }
+  }
+}
