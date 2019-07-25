31cf146317b961e92aebaf5ee2ed84b3a185ee59
johneckert <johnteckert@gmail.com>
2019-07-25 14:28:12 -0400

    INITIAL COMMIT

---
+++ chapter1.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MultipleChoice
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.write("What is your name?");
+      var name = Console.read();
+      Console.write("How old are you?")
+      var age = Console.read();
+      Console.write("In what month were you born?")
+      var month = Console.read()
+      Console.write("Name: ", name);
+      Console.write("Age: ", age);
+      Console.write("Month: ", month);
+    }
+  }
+}
